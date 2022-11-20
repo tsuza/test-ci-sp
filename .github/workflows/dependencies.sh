679183b64cb08982928bbc807d8e981bf745ff87
Suza <zabaniya101@gmail.com>
2022-11-20 18:37:17 +0100

    ci

--- .github/workflows/dependencies.sh
+++ .github/workflows/dependencies.sh
@@ -1,6 +1,7 @@
-# If you want to download one specific include file, use wget.
+# If you want to download one specific include file, use wget and utilize the Raw link of the file 
+# ( click the file and there will be a Raw button at the top right of the code ).
 # E.g.
-# wget "https://raw.githubusercontent.com/nosoop/SM-TFCustAttr/master/scripting/include/tf_custom_attributes.inc"
+# wget "https://raw.githubusercontent.com/alliedmodders/sourcemod/master/plugins/include/sdkhooks.inc"
 #
 # If you want to download an entire folder of a repository, 
 
