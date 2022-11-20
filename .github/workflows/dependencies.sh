0df94bceb81223606cdc19633e2d163472083ace
Suza <zabaniya101@gmail.com>
2022-11-20 18:33:39 +0100

    ci

--- .github/workflows/dependencies.sh
+++ .github/workflows/dependencies.sh
@@ -1,7 +1,13 @@
+# If you want to download one specific include file, use wget.
+# E.g.
+# wget "https://raw.githubusercontent.com/nosoop/SM-TFCustAttr/master/scripting/include/tf_custom_attributes.inc"
+#
+# If you want to download an entire folder of a repository, 
+
 wget "https://raw.githubusercontent.com/nosoop/SM-TFCustAttr/master/scripting/include/tf_custom_attributes.inc"
 
-mkdir stocksoup
-cd stocksoup
+# mkdir stocksoup
+# cd stocksoup
 
-wget "https://raw.githubusercontent.com/nosoop/stocksoup/master/var_strings.inc"
+wget "https://raw.githubusercontent.com/nosoop/stocksoup/master/var_strings.inc" -O stocksoup/var_strings.inc
 wget "https://raw.githubusercontent.com/nosoop/stocksoup/master/string.inc"
