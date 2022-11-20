bf411930f20b2954878961211660d1b63e488e74
Suza <zabaniya101@gmail.com>
2022-11-20 18:45:22 +0100

    test

--- .github/workflows/dependencies.sh
+++ .github/workflows/dependencies.sh
@@ -3,12 +3,22 @@
 # E.g.
 # wget "https://raw.githubusercontent.com/alliedmodders/sourcemod/master/plugins/include/sdkhooks.inc"
 #
+# If the include file is supposed to be in a subdirectory ( e.g. include/test/more.inc ),
+# remember to use mkdir to create the folder and then cd to change to that directory.
+# E.g.
+# mkdir test
+# cd test
+# wget ...
+#
+#
 # If you want to download an entire folder of a repository, 
 
 wget "https://raw.githubusercontent.com/nosoop/SM-TFCustAttr/master/scripting/include/tf_custom_attributes.inc"
 
-# mkdir stocksoup
-# cd stocksoup
+mkdir stocksoup
+cd stocksoup
 
-wget "https://raw.githubusercontent.com/nosoop/stocksoup/master/var_strings.inc" -O stocksoup/var_strings.inc
+wget "https://raw.githubusercontent.com/nosoop/stocksoup/master/var_strings.inc"
 wget "https://raw.githubusercontent.com/nosoop/stocksoup/master/string.inc"
+
+svn checkout https://github.com/nosoop/stocksoup/tree/master/tf
