4158c97f87a460c6373ef679920f35e0c3f31025
Suza <zabaniya101@gmail.com>
2022-11-20 17:58:55 +0100

    test new CI for dependencies

---
+++ .github/workflows/dependencies.sh
@@ -0,0 +1,7 @@
+wget "https://raw.githubusercontent.com/nosoop/SM-TFCustAttr/master/scripting/include/tf_custom_attributes.inc"
+
+mkdir stocksoup
+cd stocksoup
+
+wget "https://raw.githubusercontent.com/nosoop/stocksoup/master/var_strings.inc"
+wget "https://raw.githubusercontent.com/nosoop/stocksoup/master/string.inc"
