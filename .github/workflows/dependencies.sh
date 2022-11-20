cf1db42ebf4c9adb7703772ec1777777dde2e083
Suza <zabaniya101@gmail.com>
2022-11-20 18:49:34 +0100

    ci

--- .github/workflows/dependencies.sh
+++ .github/workflows/dependencies.sh
@@ -21,4 +21,4 @@ cd stocksoup
 wget "https://raw.githubusercontent.com/nosoop/stocksoup/master/var_strings.inc"
 wget "https://raw.githubusercontent.com/nosoop/stocksoup/master/string.inc"
 
-svn checkout https://github.com/nosoop/stocksoup/tree/master/tf
+svn ls https://github.com/nosoop/stocksoup/tree/master/tf --depth empty
